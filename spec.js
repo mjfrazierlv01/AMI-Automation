84dbf6b24716b9054c4416c57a1cd53838f607bd
System Administrator <root@Michaels-MacBook-Air.local>
2018-04-23 19:32:31 -0700

    Automation Created

--- spec.js
+++ spec.js
@@ -1,129 +1,61 @@
-// This is the QA Challenge test for Influential
+// AMI Automation for UI
 // Michael Frazier
-// 03.21.18
-// https://github.com/mjfrazierlv01/QA-Challenge
+// 04/23/18
+// https://github.com/mjfrazierlv01/AMI-Automation.git
 // All Rights Reserved
 
-describe('Influential Test', function() {
-   it('QA Challenge Test 1', function() {
+describe('UI Automation', function() {
+   it('QA Automation', function() {
 
 // This turns off the angular check in protractor
 // This is only used if you are using protractor to run these tests
 
    browser.ignoreSynchronization = true;
-   console.log();
 
-//This will navigate to the dev site of Influential
+//This will navigate to the QA site of AMI
 
-   browser.get('https://portal.dev.influential.co/');
-   expect(browser.getCurrentUrl()).toEqual('https://portal.dev.influential.co/');
+   browser.get('https://amiqaserver.com/login.aspx');
+   expect(browser.getCurrentUrl()).toEqual('https://amiqaserver.com/login.aspx');
 
-// This section will click on the get started button
-// Note: There are more than one Get Started button(s) on the page and will select only the first one found.
+// First, We will create a new user, by clicking on 'Register New Device'
+// Then click on 'Dont have a user? Sign up
+// Note: You will have to change the email address each time this script runs.
 
-   element(by.css('a[href="/signup"]')).click();
+   element(by.css('a[href="/registerDevice.aspx"]')).click();
+   // browser.sleep(5000);
+   element(by.xpath("html/body/form/div[3]/div/div/div/div/div[3]/div[2]/div/div[2]/div[1]/a")).click();
+   element(by.id('DeviceSnNew')).sendKeys('2499');
+   element(by.id('AccountName')).sendKeys('Test Account');
+   element(by.id('LoginName')).sendKeys('AMI Test 1');
+   element(by.id('Password')).sendKeys('Justin10!!');
+   element(by.id('ConfirmPassword')).sendKeys('Justin10!!');
+   element(by.id('PrimaryEmail')).sendKeys('mjfrazier@me.com');
+   element(by.id('FirstName')).sendKeys('Test');
+   element(by.id('LastName')).sendKeys('User1');
+   element(by.id('cbSecurityQuestion')).$('[value="2"]').click();
+   element(by.id('SecurityAnswer')).sendKeys('Las Vegas');
+   element(by.buttonText('Submit')).click();
+   element(by.buttonText("Continue")).click();
+   // browser.sleep(5000);
 
-// This will click on the 'Influential for Talent Management' link and start the process
-// This will enter the user details, set up a account and send an email to the user for verification
-// This will generate an error as the username and email are already set up and I skip the errors and log in.
+// This section will Register New Device
 
-   element(by.css('a[href="/signup/talent#form-anchor"]')).click();
-   element(by.id('name-first')).sendKeys('Michael');
-   element(by.id('name-last')).sendKeys('Frazier');
-   element(by.id('email')).sendKeys('mjfrazierlv01@mailinator.com');
-   element(by.id('company-name')).sendKeys('VirtualCorsair');
-   element(by.id('username')).sendKeys('mjfrazierlv01');
-   element(by.id('password')).sendKeys('Justin10!!');
-   element(by.id('password-confirm')).sendKeys('Justin10!!');
-   element(by.buttonText('Sign Up')).click();
-   browser.sleep(500);
+   browser.get('https://amiqaserver.com/login.aspx');
+   element(by.css('a[href="/registerDevice.aspx"]')).click();
+   element(by.css('input#DeviceSN')).sendKeys('2499');
+   element(by.css('input#Login')).sendKeys('AMI Test 1');
+   element(by.css('input#Pass')).sendKeys('Justin10!');
+   element(by.xpath("html/body/form/div[3]/div/div/div/div/div[3]/div[2]/div/div[1]/fieldset/div[1]/button/span")).click();
+   element(by.buttonText("Continue")).click();
+   // browser.sleep(5000);
 
-// Email Verification - Working on the solution
-// Correct way to do this is to install a service that checks email, reads and clicks on the email
-   //browser.ignoreSynchronization = true;
-   //browser.get('https://www.mailinator.com');
-   //element(by.id('inboxfield')).sendKeys('mjfrazierlv01');
-   //element(by.buttonText('Go!')).click();
-   //element(by.xpath("//*[contains(text(),'Fwd: 'Welcome To Talent!')]")).click();
-   //browser.sleep(500);
+// This section will perform a login request
 
-
-// Once Email is verified, log into the Influential site
-// The Sleep is added so the user can see the action, not recommended for use.
-
-   element(by.css('a[href*="/login"]')).click();
-   element(by.id('email')).sendKeys('mjfrazierlv01@mailinator.com');
-   element(by.id('password')).sendKeys('Justin10!!');
-   element(by.buttonText('Sign In')).click();
-   browser.sleep(500);
-
-// Once logged in, a basic test to make sure the main pages are present
-// The Sleep is added so the user can see the action, not recommended for use.
-
-// My Influencers Link in Side Bar
-// Creates a Influencer Profile
-
-   element(by.xpath("//div[@id='foundation']/div[4]/section/ul/li[2]/span")).click();
-   browser.sleep(500);
-   //element(by.buttonText('Add Influencer')).click();
-   //element(by.id('name_first')).sendKeys('Michael');
-   //element(by.id('name_last')).sendKeys('Frazier');
-   //element(by.id('email')).sendKeys('mjfrazierlv01@mailinator.com');
-   //element(by.id('phone')).sendKeys('702-555-1212');
-   //element(by.id('demo-age')).sendKeys('44');
-   //element(by.id('demo-gender')).$('[value="male"]').click();
-   //element(by.id('demo-ethnicity')).$('[value="black"]').click();
-   //element(by.id('demo-location')).sendKeys('Las Vegas').click();
-   //element(by.id('vertical-search')).$('[value="Gaming"]');
-   //element(by.id('vertical-search')).sendKeys('Gaming');
-   //element(by.id('facebook-search')).sendKeys('www.facebook.com/michael.frazier');
-   //element(by.id('twitter-search')).sendKeys('www.twitter.com/michael.frazier');
-   //element(by.id('instagram-search')).sendKeys('www.instagram.com/michael.frazier');
-   //element(by.id('youtube-search')).sendKeys('www.youtube.com/michael.frazier');
-   //element(by.id('snapchat-search')).sendKeys('www.snapchat.com/michael.frazier');
-   //element(by.buttonText('Add Influencer')).click();
-
-
-// Opportunities Link in Side Bar
-     element(by.xpath("//div[@id='foundation']/div[4]/section/ul/li[3]/span")).click();
-     browser.sleep(50);
-
-// Offers Link in Side Bar
-     element(by.xpath("//div[@id='foundation']/div[4]/section/ul/li[4]/span")).click();
-     browser.sleep(50);
-
-// Social Tracker Link in Side Bar
-     element(by.xpath("//div[@id='foundation']/div[4]/section/ul/li[5]/span")).click();
-     browser.sleep(50);
-
-// Content Search Link in Side Bar
-     element(by.xpath("//div[@id='foundation']/div[4]/section/ul/li[6]/span")).click();
-     browser.sleep(50);
-
-// Roster Link in Side Bar
-     element(by.xpath("//div[@id='foundation']/div[4]/section/ul/li[7]/span")).click();
-     browser.sleep(50);
-
-// Team Link in Side Bar
-     element(by.xpath("//div[@id='foundation']/div[4]/section/ul/li[8]/span")).click();
-     browser.sleep(5000);
-
-// Click on Notifications
-     element(by.css('i.fa.fa-bell')).click();
-     browser.sleep(500);
-
-// Click on the Gear Icon - my Profile
-     element(by.css('i.fa.fa-cog')).click();
-     element(by.linkText('My Profile')).click();
-     browser.sleep(500);
-
-// Click on the Gear Icon - Logout
-     element(by.css('i.fa.fa-cog')).click();
-     element(by.linkText('Logout')).click();
-     browser.sleep(500);
-
-// This section will be reserved to gather credentials from local database and
-// and use them on the site.
-
+   browser.get('https://amiqaserver.com/login.aspx');
+   element(by.id('username')).sendKeys('AMI Test 1');
+   element(by.id('Password')).sendKeys('Justin10!');
+   element(by.xpath("/html/body/form/div[3]/div/div/div/div/div[2]/div[1]/div/div[1]/fieldset/div[1]/button/span")).click();
+   element(by.buttonText("Login")).click();
+   // browser.sleep(5000);
     });
 });
